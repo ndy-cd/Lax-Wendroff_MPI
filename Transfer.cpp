1543ee79f9d4d555aa95f22a3b0dfd9aa1b18017
nody <nody.email@gmail.com>
2019-12-09 15:58:42 +0500

    Transfer works, bounds works, new void BoundExchange added

--- Transfer.cpp
+++ Transfer.cpp
@@ -9,6 +9,7 @@ int writeFile (double[], int, char*);
 void getFlow (double U[], double F[], int n, double velocity);
 void LW (double U[], double F[], double fun, double dt, double dx, int N);
 int boundExchange(double &left, double &right, int size, int rank, MPI_Request request, MPI_Status status);
+void BoundExchange(double U[], double F[], int n, int size, int rank, MPI_Status status);
 
 int main (int argc, char** argv){
     char fileName[20] = "file1.txt", add[5] = "a", create[5] = "w"; 
@@ -32,7 +33,7 @@ int main (int argc, char** argv){
         while (N % size != 0) N++; 
         // Nlocal = (N / size) + 1;    //количество ячеек и размер массива !!
         // N=Nlocal*size;              //выравнивание
-        N = 20;
+        N = 40;
         printf("Parameters are readed. Total mesh count is %d\n", N);
         Uglobal = new double[N];
         for (int i = 0; i < N/10; i++) {    // начальные условия (ступенька)
@@ -54,31 +55,33 @@ int main (int argc, char** argv){
     MPI_Scatter((void*)Uglobal, n, MPI_DOUBLE, (void*)&U[1], n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     U[0] = U[1];
-    U[n] = 2;
     U[n+1] = U[n];
-    // if (rank == 0) MPI_Send((void*)&U[n+1], 1, MPI_DOUBLE, 1, 123, MPI_COMM_WORLD);  
-    // if (rank == 1) MPI_Recv((void*)&U[0], 1, MPI_DOUBLE, 0, 123, MPI_COMM_WORLD, &status);  
     boundExchange(U[0], U[n+1], size, rank, request, status);
     getFlow(U, F, n, velocity);
-    printf("left bound = %f\tright bound = %f\tcenter = %f\trank = %d\n", U[0], U[n+1], U[n/2], rank);
+    // printf("left bound = %f\tright bound = %f\tcenter = %f\trank = %d\n", U[0], U[n+1], U[n/2], rank);
     
     // вычисление нового временного слоя
-    while (dt < 10)
+    while (dt < 30)
     {
         LW(U, F, velocity, 1, 1, n);
         dt++;
-        // U[n] = U[n-1];
-        boundExchange(U[0], U[n], size, rank, request, status);
+        BoundExchange(U, F, n, size, rank, status);
+        /*              COMBINED TO void BoundExchange
+        U[n+1] = U[n];
+        F[n+1] = F[n];
+        boundExchange(U[0], U[n+1], size, rank, request, status);
+        boundExchange(F[0], F[n+1], size, rank, request, status);
         if (rank == 0) {
-            U[0] = U[1];
-            printf("U[n] = %f, U[n-1] = %f h = %d\n", U[n], U[n-1], dt);
+            U[0] = U[1]; //не используется в void BoundExchange
+            // printf("U[n] = %f, U[n-1] = %f h = %d\n", U[n], U[n-1], dt);
             // printf("F[n] = %f, F[n-1] = %f h = %d\n", F[n], F[n-1], dt);
         } 
         else {
             U[1] = U[0];
-            if (rank == 1)
-            printf("rank %d, left boundary %f, first mesh = %f\n", rank, U[0], U[1]);
-        }
+            F[1] = F[0];            
+            // if (rank == 1) printf("rank %d, left boundary %f, first mesh = %f\n", rank, U[0], U[1]);
+        } */
+
         if (dt % 1 == 0){
             // сборка данных (gather)
             MPI_Gather((void*)U, n, MPI_DOUBLE, (void*)Uglobal, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -124,12 +127,12 @@ void LW (double U[], double F[], double fun, double dt, double dx, int N){
     double* U12 = new double [N];
     double* F12 = new double [N];
 
-    for (int i = 0; i < N; i++) {                     // вспомогательный шаг
+    for (int i = 0; i < N+1; i++) {                     // вспомогательный шаг
     	U12[i] = (U[i] + U[i+1])/2 - dt/2/dx*(F[i+1] - F[i]);
     	F12[i] = fun*U12[i];
     }
     // U[1] -= dt/dx*(F12[1]);
-    for (int i = 0; i < N; i++) {                     // основной шаг
+    for (int i = 1; i < N+1; i++) {                     // основной шаг
     	U[i] -= dt/dx*(F12[i]-F12[i-1]);
     	F[i] = U[i]*fun;
     }
@@ -176,8 +179,34 @@ int boundExchange(double &left, double &right, int size, int rank, MPI_Request r
             MPI_Recv((void*)&left, 1, MPI_DOUBLE, i-1, 123, MPI_COMM_WORLD, &status);  
             // printf("rank %d recieved left bound as %f\n", rank, left);
         }
-    }
+    }    
     return 0;
+}
+
+void BoundExchange(double U[], double F[], int n, int size, int rank, MPI_Status status) {
+    // лучше инициализировать size, rank, status, или передать?
+    printf("BE init! rank %d\n", rank);
+    U[n+1] = U[n];
+    F[n+1] = F[n];
+    for (int i = 0; i < size-1; i++)
+    {
+        if (rank == i) {
+            MPI_Send((void*)&U[n+1], 1, MPI_DOUBLE, i+1, 123, MPI_COMM_WORLD);
+            MPI_Send((void*)&F[n+1], 1, MPI_DOUBLE, i+1, 321, MPI_COMM_WORLD);
+        }
+    }
+    for (int i = 1; i < size; i++)
+    {
+        if (rank == i) {
+            MPI_Recv((void*)&U[0], 1, MPI_DOUBLE, i-1, 123, MPI_COMM_WORLD, &status);
+            MPI_Recv((void*)&F[0], 1, MPI_DOUBLE, i-1, 321, MPI_COMM_WORLD, &status);
+            U[1] = U[0];
+            F[1] = F[0];
+            printf("BE recv! rank %d\n", rank);
+
+        }
+    }
+    printf("BE works! rank %d\n", rank);
 }
 
 int writeFile (double A[], int N, char* option) {
